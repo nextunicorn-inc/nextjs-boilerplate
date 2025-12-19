c52106aa69b446a5f756916ca3af8a4dbf3028ad
Cham <103615923+cham-sim@users.noreply.github.com>
2025-12-19 12:13:55 +0900

    실행 에러 수정

---
+++ api/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          // GA4 데이터를 읽기 위한 권한 요청
+          scope:
+            "openid email profile https://www.googleapis.com/auth/analytics.readonly",
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  // 보안을 위해 Secret 설정
+  secret: process.env.NEXTAUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
